44f6c0b3a83bd7412f2927f8c1adde534066d139
Andrew Morton <ammorton22@gmail.com>
2022-05-30 21:22:14 +0100

    Show page with basic information for each card

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -2,4 +2,11 @@ class DecksController < ApplicationController
   def index
     @decks = Deck.all
   end
+
+  def show
+    @deck = Deck.find(params[:id])
+    @rating = Rating.new
+    @cards = Card.new
+  end
+
 end
