b6c51b3ec30f9568a11726d882d525fa2956b687
Andrew Morton <ammorton22@gmail.com>
2022-06-03 12:34:50 +0100

    Able to add multiple questions to one form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require('./nested-forms/addFields')
+require('./nested-forms/removeFields')
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
