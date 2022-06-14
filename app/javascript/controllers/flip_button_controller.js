d1467085048f9fa44af00add477500f4dc2050ad
Andrew Morton <ammorton22@gmail.com>
2022-06-14 19:39:28 +0100

    Buttons now working

---
+++ app/javascript/controllers/flip_button_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "button", "card" ]
+
+  connect() {
+    console.log("Hello from our first Stimulus controller")
+  }
+
+  flip() {
+    console.log("Working")
+    console.log(this.cardTarget)
+    // this.cardTarget.classList.toggle("is-flipped")
+    document.querySelector('.active').firstElementChild.firstElementChild.classList.toggle("is-flipped")
+    document.querySelector('.active').previousElementSibling.firstElementChild.firstElementChild.classList.remove("is-flipped")
+  }
+}
