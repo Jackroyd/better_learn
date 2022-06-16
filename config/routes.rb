c8af27dc845e47e84d9261bbcdd9a80083437e89
Andrew Morton <ammorton22@gmail.com>
2022-06-16 20:44:20 +0100

    simple profile page with the ability to edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-    resources :decks do
-      resources :cards, only: [ :new, :create, :edit, :update, :index ]
-    end
+  resources :users, only: [ :show, :edit, :update ]
+  resources :decks do
+    resources :cards, only: [ :new, :create, :edit, :update, :index ]
+  end
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
