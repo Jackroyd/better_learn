d99052d2d1a19f3a3091a7732e602ff7873c5310
Andrew Morton <ammorton22@gmail.com>
2022-06-22 09:01:10 +0100

    Working on pulling through the correct user statistics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,13 @@ class PagesController < ApplicationController
   end
 
   def profile
+    @progress_logs = ProgressLog.all
+    # Find a count of the project log details rows where
+    # progress log ids match progress logs with the
+    # user_id = current user
+    @progress_log_details = ProgressLogDetail.all
+    # Find a count of all instances where
+    # progress log details.correct = true
+    # / progress log detials.correct = false
   end
 end
