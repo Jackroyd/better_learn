481328f2b17c502b7ce4596cfb39efac54a6bfcb
Josh Ackroyd <ackroydjosh93@gmail.com>
2022-06-24 17:25:35 +0100

    fixed the filters, removed topics

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,24 +2,9 @@ class Deck < ApplicationRecord
   belongs_to :user
   has_many :cards, dependent: :destroy
   has_many :ratings, dependent: :destroy
-  validates :name, :subject, :topic, :level, presence: true
-  validates :subject, inclusion: { in: %w[science history maths] }
-  validates :topic, inclusion: { in: %w[biology chemistry physics] }
-  validates :level, inclusion: { in: %w[ks1 ks2 gcse] }
+  validates :name, :subject, :level, presence: true
   validates :name, uniqueness: { scope: :user }
   accepts_nested_attributes_for :cards, allow_destroy: true, reject_if: :all_blank
-
-  def self.by_sub_top_lev(subject = nil, topic = nil, level = nil)
-    return where(subject: subject, topic: topic, level: level) if subject && topic && level
-    return where(topic: topic, level: level) if topic && level
-    return where(subject: subject, level: level) if subject && level
-    return where(subject: subject, topic: topic) if subject && topic
-    return where(subject: subject) if subject
-    return where(topic: topic) if topic
-    return where(level: level) if level
-
-    all
-  end
 
   include PgSearch::Model
   pg_search_scope :decks_search,
@@ -30,4 +15,11 @@ class Deck < ApplicationRecord
     using: {
       tsearch: { prefix: true }
     }
+
+  pg_search_scope :filter_search,
+    against: [ :subject, :level],
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,50 @@
-require 'faker'
-# seeding 5 users, 10 decks with random question answers
+# require 'faker'
+# # seeding 5 users, 10 decks with random question answers
 
-# delete all current users
-User.destroy_all
-puts "users destroyed"
-user_list = []
-# seeding users:
-User.create!(first_name: "demo", last_name: "user", email: "demo@user.com", password: "password")
-puts "test user created. email - demo@user.com, password - password"
-5.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = "#{first_name}@#{last_name}.com"
-  password = "password"
-  User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
-  user_list << User.last.id
-  puts "User #{email} created"
-end
-puts "all users created"
-# delete all current decks
-Deck.destroy_all
+# # delete all current users
+# User.destroy_all
+# puts "users destroyed"
+# user_list = []
+# # seeding users:
+# User.create!(first_name: "demo", last_name: "user", email: "demo@user.com", password: "password")
+# puts "test user created. email - demo@user.com, password - password"
+# 5.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   email = "#{first_name}@#{last_name}.com"
+#   password = "password"
+#   User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
+#   user_list << User.last.id
+#   puts "User #{email} created"
+# end
+# puts "all users created"
+# # delete all current decks
+# Deck.destroy_all
 
-10.times do
-  name = Faker::Educator.course_name
-  user = User.find(user_list.sample)
-  description = Faker::Quotes::Shakespeare.hamlet_quote
-  subject = ["science", "history", "maths"].sample
-  topic = ["biology", "physics", "chemistry"].sample
-  level = ["ks1", "ks2", "gcse"].sample
-  Deck.create!(name: name, user: user, subject: subject, topic: topic, level: level, description: description)
-  rating = rand(1..5)
-  Rating.create!(rating: rating, user: User.find(user_list.sample), deck: Deck.last)
-  puts "Created deck '#{name}'"
-end
-puts "decks seeded"
+# 10.times do
+#   name = Faker::Educator.course_name
+#   user = User.find(user_list.sample)
+#   description = Faker::Quotes::Shakespeare.hamlet_quote
+#   subject = ["science", "history", "maths"].sample
+#   topic = ["biology", "physics", "chemistry"].sample
+#   level = ["ks1", "ks2", "gcse"].sample
+#   Deck.create!(name: name, user: user, subject: subject, topic: topic, level: level, description: description)
+#   rating = rand(1..5)
+#   Rating.create!(rating: rating, user: User.find(user_list.sample), deck: Deck.last)
+#   puts "Created deck '#{name}'"
+# end
+# puts "decks seeded"
 
-puts "adding cards to decks"
-Deck.all.each do |deck|
-  10.times do
-    question = "Who said '#{Faker::GreekPhilosophers.quote}'"
-    answer = Faker::GreekPhilosophers.name
-    Card.create!(question: question, answer: answer, deck: deck)
-  end
-end
+# puts "adding cards to decks"
+# Deck.all.each do |deck|
+#   10.times do
+#     question = "Who said '#{Faker::GreekPhilosophers.quote}'"
+#     answer = Faker::GreekPhilosophers.name
+#     Card.create!(question: question, answer: answer, deck: deck)
+#   end
+# end
 
-puts "seeding complete"
+# puts "seeding complete"
 
 #######################################################
 # require "nokogiri"
@@ -71,13 +71,56 @@ puts "seeding complete"
 #   end
 # end
 
-# url = "https://www.flashcardmachine.com/religion-chapter19.html#cards"
-# content = URI.open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera')
-# doc = Nokogiri::HTML(content)
+# require 'faker'
+# # seeding 20 users
 
-# doc.search('tr')[1..].each do |card|
-#   card.search("td tr td b").each_slice(2) do |q,a|
-#     puts "Q: #{q}"
-#     puts "A: #{a}"
-#   end
+# # delete all current users
+# User.destroy_all
+# puts "users destroyed"
+# user_list = []
+# # seeding users:
+# User.create!(first_name: "demo", last_name: "user", email: "demo@user.com", password: "password")
+# puts "test user created. email - demo@user.com, password - password"
+# 20.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   email = "#{first_name}@#{last_name}.com"
+#   password = "password"
+#   User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
+#   user_list << User.last.id
+#   puts "User #{email} created"
 # end
+# puts "all users created"
+
+
+# # creating decks
+# puts "creating decks"
+# deck_list = []
+url = "https://www.flashcardmachine.com/flashcards.html"
+content = URI.open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera')
+doc = Nokogiri::HTML(content)
+
+doc.search(".fcStaticSubject ul li a").each do |sub|
+  href = sub['href']
+  suburl = "https://www.flashcardmachine.com/#{href}"
+  subcontent = URI.open(suburl, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera')
+  subdoc = Nokogiri::HTML(subcontent)
+
+  subdoc.search(".fcStaticSubject ul li a").each do |set|
+    href = set['href']
+    seturl = "https://www.flashcardmachine.com/#{href}"
+    setcontent = URI.open(seturl, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera')
+    setdoc = Nokogiri::HTML(setcontent)
+    title = setdoc.search('.form_item')[0].search('.field').inner_text
+    description = setdoc.search('.form_item')[1].search('.field').inner_text
+    subject = setdoc.search('.form_item')[3].search('.field').inner_text
+    level = setdoc.search('.form_item')[4].search('.field').inner_text
+    puts title
+    setdoc.search('tr')[1..].each do |card|
+      card.search("td tr td b").each_slice(2) do |q, a|
+        puts "Q: #{q.inner_text}"
+        puts "A: #{a.inner_text}"
+      end
+    end
+  end
+end
